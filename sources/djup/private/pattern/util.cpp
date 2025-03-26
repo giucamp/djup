105d1bba47362c4f06d75609d7b537aed29f1e5f
giucamp <giu.campana@gmail.com>
2025-03-26 12:55:46 +0100

    pattern match refactoring

---
+++ sources/djup/private/pattern/util.cpp
@@ -0,0 +1,49 @@
+
+//   Copyright Giuseppe Campana (giu.campana@gmail.com) 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//        (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <private/pattern/utils.h>
+#include <private/pattern/pattern_info.h>
+
+namespace djup
+{
+    namespace pattern
+    {
+        std::vector<Span<const Tensor>> Tokenize(const Tensor& i_tensor)
+        {
+            std::vector<Span<const Tensor>> result;
+            result.push_back({ &i_tensor, 1 });
+
+            for (size_t i = 0; i < result.size(); i++)
+            {
+                Span<const Tensor> arguments = result[i];
+
+                for (size_t j = 0; j < arguments.size(); j++)
+                {
+                    if (!IsLiteral(arguments[j]) && !IsIdentifier(arguments[j]) && !IsRepetition(arguments[j]))
+                    {
+                        result.push_back(Span(arguments[j].GetExpression()->GetArguments()));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        std::string TensorSpanToString(Span<const Tensor> i_tensor)
+        {
+            std::string result;
+            for (size_t i = 0; i < i_tensor.size(); i++)
+            {
+                if (i != 0)
+                    result += ", ";
+                result += ToSimplifiedStringForm(i_tensor[i]);
+            }
+            return result;
+        }
+
+    } // namespace pattern
+
+} // namespace djup
---
+++ sources/djup/private/pattern/utils.h
@@ -0,0 +1,18 @@
+
+//   Copyright Giuseppe Campana (giu.campana@gmail.com) 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//        (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#include <djup/tensor.h>
+
+namespace djup
+{
+    namespace pattern
+    {
+
+
+    } // namespace pattern
+
+} // namespace djup
