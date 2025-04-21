23654f23b8f0624597fe0607f103281f23165f82
giucamp <giu.campana@gmail.com>
2025-04-21 10:48:42 +0200

    added some doc

--- sources/core/public/core/flags.h
+++ sources/core/public/core/flags.h
@@ -10,6 +10,13 @@
 
 namespace core
 {
+    /** Performs the bitwise for an enum type. This function can be used to implement 
+        a bitwise or operator for an enum type:
+        
+        constexpr FLAGS operator | (FLAGS i_first, FLAGS i_second)
+            { return CombineFlags(i_first, i_second); }
+
+        where FLAGS is an enum type. */
     template <typename ENUM_TYPE, typename = std::enable_if_t<std::is_enum_v<ENUM_TYPE>>>
         constexpr ENUM_TYPE CombineFlags(ENUM_TYPE i_first, ENUM_TYPE i_second)
     {
@@ -17,6 +24,8 @@ namespace core
         return static_cast<ENUM_TYPE>(static_cast<UnderlyingType>(i_first) | static_cast<UnderlyingType>(i_second));
     }
 
+    /** Checks if a single bit is set in an enum type. If i_flag
+        has more that one bit set, the behavior is undefined. */
     template <typename ENUM_TYPE, typename = std::enable_if_t<std::is_enum_v<ENUM_TYPE>>>
         constexpr bool HasFlag(ENUM_TYPE i_source, ENUM_TYPE i_flag)
     {
@@ -30,6 +39,8 @@ namespace core
         return (source & flag) != 0;
     }
 
+    /** Checks if all the bits in i_flags are set in i_source. 
+        ENUM_TYPE must be an enum type. */
     template <typename ENUM_TYPE, typename = std::enable_if_t<std::is_enum_v<ENUM_TYPE>>>
         constexpr bool HasAllFlags(ENUM_TYPE i_source, ENUM_TYPE i_flags)
     {
@@ -39,6 +50,8 @@ namespace core
         return (source & flags) == flags;
     }
 
+    /** Checks if any of the bits in i_flags is set in i_source.
+        ENUM_TYPE must be an enum type. */
     template <typename ENUM_TYPE, typename = std::enable_if_t<std::is_enum_v<ENUM_TYPE>>>
         constexpr bool HasAnyFlag(ENUM_TYPE i_source, ENUM_TYPE i_flags)
     {
